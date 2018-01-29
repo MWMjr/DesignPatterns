a138ef152f850d18865a538e4add5878a61374fc
Michael W. Montageu Jr <michaelmontaguejr@gmail.com>
2018-01-29 00:47:27 -0600

    Fixing issues with testing - removing nunit, using standard .NET test infrastructure

--- MWM.DesignPatterns/MWM.DesignPatterns/Singleton/Main.cs
+++ MWM.DesignPatterns/MWM.DesignPatterns/Singleton/Main.cs
@@ -1,10 +1,14 @@
-﻿using MoreLinq;
-using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
+using MoreLinq;
+using static System.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MWM.DesignPatterns.Singleton
 {
@@ -64,7 +68,7 @@ namespace MWM.DesignPatterns.Singleton
             instanceCount++;
             Console.WriteLine("Initializing database");
 
-            capitals = System.IO.File.ReadAllLines("capitals.txt")
+            capitals = System.IO.File.ReadAllLines(System.IO.Path.Combine(new System.IO.FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "capitals.txt"))
                 .Batch(2)
                 .ToDictionary(
                     list => list.ElementAt(0).Trim(),
@@ -82,16 +86,16 @@ namespace MWM.DesignPatterns.Singleton
         public static SingletonDatabase Instance => instance.Value;
     }
 
-    [TestFixture]
+    [TestClass]
     public class SingletonTest
     {
-        [Test]
+        [TestMethod]
         public void IsSingletonTest()
         {
             var db = SingletonDatabase.Instance;
             var db2 = SingletonDatabase.Instance;
-            Assert.That(db, Is.SameAs(db2));
-            Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+            Assert.Equals(db, db2);
+            Assert.Equals(SingletonDatabase.Count, 1);
         }
     }
 
