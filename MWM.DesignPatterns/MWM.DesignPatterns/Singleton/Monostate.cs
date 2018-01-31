f1213095df140725705872d7ac4a70b64309f63d
Michael W. Montageu Jr <michaelmontaguejr@gmail.com>
2018-01-30 23:38:49 -0600

    Adding a monostate version of a singleton, where the properties are the statics

---
+++ MWM.DesignPatterns/MWM.DesignPatterns/Singleton/Monostate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWM.DesignPatterns.Singleton
+{
+    //Here the static variables mean all instances of the CEO class share the same value for certain properties
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(age)}: {Age}";
+        }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+            System.Console.WriteLine(ceo2);
+
+        }
+    }
+}
