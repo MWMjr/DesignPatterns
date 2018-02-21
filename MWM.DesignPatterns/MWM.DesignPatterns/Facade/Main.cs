28dc4bc0ae9e1149002beb101ed2a0a742c61345
Michael W. Montageu Jr <michaelmontaguejr@gmail.com>
2018-02-21 00:56:06 -0600

    Adding facade

---
+++ MWM.DesignPatterns/MWM.DesignPatterns/Facade/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWM.DesignPatterns.Facade
+{
+    // Provies a simplified API over a set of classes / subsystems
+
+    // May wish to optionally expost interals through the facade  - typically something for power users to use
+
+    // Currently don't have an example
+    // Facade depends on the subsystem, the whole point of it is to show a simplofied interface for something that
+    // is very complicated
+
+    // Need to find an example of this out in the wild
+    class Main
+    {
+    }
+}
